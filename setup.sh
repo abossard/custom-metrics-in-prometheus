cd18f5cb55c2f2093426e66f8e3ee56b212db51b
Andre Bossard <anbossar@microsoft.com>
2022-01-18 14:03:04 +0100

    init

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#SETUP
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f values.yaml
+
+mkdir hello-func
+cd hello-func
+func init --docker --csharp
+
+
+#CONFIG LOCAL
+POD=$(kubectl get pod -n prometheus -l app.kubernetes.io/name=grafana -o jsonpath='{.items[0].metadata.name}')
+ACR=anbossar
+echo $POD
+kubectl port-forward -n prometheus $POD 3000
+echo Login to Grafana with admin / prom-operator
+echo https://localhost:3000
+
+#TEST BUILD function app
+cd hello-func
+func start
+docker build -t anbossar.azurecr.io/prom-func-app:latest .
+docker buildx build --platform linux/amd64 -t anbossar.azurecr.io/prom-func-app:latest --push .
+az acr build --registry $ACR --image prom-func-app:latest .
+
+az acr login -n $ACR
+az acr update --name anbossar --anonymous-pull-enabled
+
+cd webapi
+docker buildx build --platform linux/amd64 -t anbossar.azurecr.io/prom-webapi:latest --push .
+az acr build --registry $ACR --image prom-webapi:latest .
